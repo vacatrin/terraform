437b613adcaf76950cd5c034530bc0062b6f4f9c
Victor Acatrinei <victor.acatrinei@thoughtworks.com>
2025-09-26 17:06:45 +0300

    up to section 1, lesson 23

--- section_1_deploying_infra_w_terraform/github.tf
+++ section_1_deploying_infra_w_terraform/github.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    github = {
-      source  = "integrations/github"
-      version = "~> 6.0"
-    }
-  }
-}
-
 # Configure the GitHub Provider
 provider "github" {
   token = var.github_token
